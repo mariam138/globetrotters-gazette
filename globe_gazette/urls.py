29c5980b7bda76da0a0a9d2c00591301bcee3dbe
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-08 18:20:20 +0000

    import index url

--- globe_gazette/urls.py
+++ globe_gazette/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from blog import views as blog_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', blog_views.globe_gazette, name="index"),
 ]
