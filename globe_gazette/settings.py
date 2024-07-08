d2625ed420186ca85ace069c0e977c5cb4dd3a71
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-08 17:37:17 +0000

    connect CI database to project

--- globe_gazette/settings.py
+++ globe_gazette/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yvqs*=%#i*7kp1ppovv!9@nl_h)x!k=7h+gw36r4_%x^5)f^&('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'globe_gazette.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import os
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
