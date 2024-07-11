39715b456d4b4ce0beb6a02941f19ff7884ec68a
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-11 13:57:42 +0000

    create path for static files

--- globe_gazette/settings.py
+++ globe_gazette/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
