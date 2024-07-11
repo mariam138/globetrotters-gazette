14243011e0f2754bfb137e2412ad7fb5c356fac8
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-11 12:23:42 +0000

    add bootstrap css and js links

--- globe_gazette/settings.py
+++ globe_gazette/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'globe_gazette.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
