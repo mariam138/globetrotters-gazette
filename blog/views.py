1637ca61ffe3e6eb771219d586c9ae333f3f4337
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-11 13:35:54 +0000

    set up simple view for index.html

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def globe_gazette(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'blog/index.html')
