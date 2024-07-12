b1cc5c815b07ff022233f464da75a96adcfdd655
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-12 14:51:16 +0000

    add path for allauth urls

--- globe_gazette/urls.py
+++ globe_gazette/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 # from blog import views
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     # path('', views.globe_gazette, name="index"),
     path("", include("blog.urls"), name="blog-urls"),
