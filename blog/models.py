e3f36ea864885e6d281c6c647cf1afbd564004e2
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-13 13:54:25 +0000

    create Post model

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,22 @@ from django.db import models
 
 # Create your models here.
 
+REGIONS = [
+    ('US', 'USA / Canada'),
+    ('SA', 'South /Central America'),
+    ('AUS', 'Australasia'),
+    ('AF', 'Africa'),
+    ('EU', 'Europe / UK'),
+    ('MENA', 'Middle East')
+]
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField()
+    region = models.CharField(choices=REGIONS)
+    country = models.CharField()
+    slug = models.SlugField()
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
