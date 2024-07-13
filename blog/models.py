e3479b896588cd66a758884952907cf8bf3ea0d1
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-13 13:46:17 +0000

    create profile_page app + move profile model across

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,5 @@
 from django.db import models
-# takes information from User model in custom models
-from django.contrib.auth.models import User
-# uses cloudinary model to post images rather than django
-from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField()
-    birthday = models.DateField()
-    bio = models.TextField()
-    profile_pic = CloudinaryField('image')
-    instagram = models.URLField()
-    twitter_x = models.URLField()
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
