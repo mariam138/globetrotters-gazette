7a54ed040837b575bdee68f9bc86b9c7cfcb1ac9
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-13 13:40:19 +0000

    create Profile model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# takes information from User model in custom models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField()
+    birthday = models.DateField()
+    bio = models.TextField()
+    profile_pic = CloudinaryField('image')
+    instagram = URLField()
+    twitter_x = URLField()
+    posts = ForeignKey(Post, on_delete=models.CASCADE)
