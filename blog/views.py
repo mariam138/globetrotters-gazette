6f0981a94b70b960a68a52825cf6cd83641f87aa
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-14 15:59:51 +0000

    add success message for registration

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+    # display success message when user has registered
+    messages.add_message(request, messages.SUCCESS, 'Registration successful!')
