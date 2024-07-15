b6f6ccd1c5bdf55204977bf5deeda1f0086cca98
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-15 18:27:43 +0000

    add summernote editor to bio field in Profile

--- profile_page/admin.py
+++ profile_page/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Profile
 
+# add summernote to Profile bio text field in admin panel
+class ProfileAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
