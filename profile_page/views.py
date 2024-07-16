401b4554180b00d1da0f42f177fa2f2202340c0b
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-16 19:44:22 +0000

    create profile context to display user's info

--- profile_page/views.py
+++ profile_page/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
 
+# decorator is used to ensure that only user's who are logged in
+# can see this view. unauthorised users are redirected to login
+# code from:
+# https://docs.djangoproject.com/en/5.0/topics/auth/default/#the-login-required-decorator
+@login_required
 def profile_page(request):
-    profile = Profile.objects.all()
+    try:
+        # Get the profile for the logged-in user
+        profile = Profile.objects.get(user=request.user)
+    # Handle case where profile does not exist
+    except Profile.DoesNotExist:
+        profile = None
+
     return render(request, 'profile_page/profile_page.html',
     {
-        'profile': profile}
-    
+        'profile': profile,
+    },)
