954b772f18fa739a734c8414917624d910e018c2
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-16 19:16:51 +0000

    add profile context

--- profile_page/views.py
+++ profile_page/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 
 def profile_page(request):
-    return render(request, 'profile_page/profile_page.html')
+    profile = Profile.objects.all()
+    return render(request, 'profile_page/profile_page.html',
+    {
+        'profile': profile}
+    
