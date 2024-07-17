63b2d732c23773752ed5b284217d092251b8becc
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-17 17:50:27 +0000

    add styling to email info button

--- globe_gazette/settings.py
+++ globe_gazette/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = [
     '8000-mariam138-globetrotters-1mhio5zxo6c.ws.codeinstitute-ide.net',
     '.herokuapp.com',
