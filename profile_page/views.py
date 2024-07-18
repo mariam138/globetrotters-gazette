1563191cdc26d20782be93ce45d314a2e8f7d375
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-18 16:58:36 +0000

    use get_object_or_404 method to get profile

--- profile_page/views.py
+++ profile_page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile
@@ -11,12 +11,16 @@ from .models import Profile
 # https://docs.djangoproject.com/en/5.0/topics/auth/default/#the-login-required-decorator
 @login_required
 def profile_page(request):
-    try:
-        # Get the profile for the logged-in user
-        profile = Profile.objects.get(user=request.user)
-    # Handle case where profile does not exist
-    except Profile.DoesNotExist:
-        profile = None
+    # try:
+    #     # Get the profile for the logged-in user
+    #     profile = Profile.objects.get(user=request.user)
+    # # Handle case where profile does not exist
+    # except Profile.DoesNotExist:
+    #     profile = None
+
+    # use the get_object_or_404 method to return a 404 page if profile is not found
+    queryset = Profile.objects.get(user=request.user)
+    profile = get_object_or_404(queryset)
 
     return render(request, 'profile_page/profile_page.html',
     {
