808ba762c5e3534cd8a1b06e60ac8aa1c1275ed4
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-19 21:00:42 +0000

    add render method to render widget as html string

--- profile_page/widgets.py
+++ profile_page/widgets.py
@@ -2,4 +2,9 @@
 from django.forms.widgets import ClearableFileInput
 
 class CloudinaryFileInput(ClearableFileInput):
-    pass
+    template_name = 'profile_page/cloudinary_file_input.html'
+
+    def render(self, name, value, attrs=None, renderer=None):
+        """Render the widget as an HTML string."""
+        context = self.get_context(name, value, attrs)
+        input_html = self._render(self.template_name, context, renderer)
