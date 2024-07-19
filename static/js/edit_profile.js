8aafd1c3b7bcfdd4448cd08bdd791766a75809f5
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-19 17:22:15 +0000

    create widget for profile pic upload

--- static/js/edit_profile.js
+++ static/js/edit_profile.js
@@ -1,2 +1,10 @@
 // Set cloud name for Cloudinary
 cloudinary.setCloudName("dy1xfelbe");
+
+// Create the widget for profile picture upload
+let profilePicWidget = cloudinary.createUploadWidget({
+    uploadPreset: 'profile_img',
+    cropping: true
+}, (error, result) => {
+    console.log(error, result)
+})
