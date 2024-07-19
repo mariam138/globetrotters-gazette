7f0dbfde04608aaa5165c607ee48a0ed12c630f1
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-19 17:32:06 +0000

    add event listener to open upload widget for profile img

--- static/js/edit_profile.js
+++ static/js/edit_profile.js
@@ -1,5 +1,7 @@
 // Set cloud name for Cloudinary
 cloudinary.setCloudName("dy1xfelbe");
+// Gets the input for uploading a profile picture
+let uploadProfilePic = document.getElementById('id_profile_pic');
 
 // Create the widget for profile picture upload
 let profilePicWidget = cloudinary.createUploadWidget({
@@ -7,4 +9,10 @@ let profilePicWidget = cloudinary.createUploadWidget({
     cropping: true
 }, (error, result) => {
     console.log(error, result)
-})
+});
+
+// Create event listener to open the widget when the button is clicked
+uploadProfilePic.addEventListener("click", () => {
+        profilePicWidget.open();
+    },
+    false);
