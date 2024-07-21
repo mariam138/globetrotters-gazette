665422020484f76d9e4c40a87d1457687b744f7a
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-21 16:21:05 +0000

    create url path for cancelling profile changes

--- profile_page/urls.py
+++ profile_page/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("<str:username>/", views.profile_page, name="profile_page"),
     path("<str:username>/edit", views.edit_save_profile, name="edit_profile"),
+    path("<str:username>/cancel", views.edit_cancel_profile, name="cancel_profile_changes"),
 ]
