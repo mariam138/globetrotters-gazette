10c39f78b630df55db50fec3acdf584e953ab6a7
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-23 21:29:29 +0000

    register PostAdmin class

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Post
 
 # Prepopulates the slug field from the post's title
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ["title"]}
+    prepopulated_fields = {"slug": ("title",)}
 
 # Register your models here.
 admin.site.register(Post)
