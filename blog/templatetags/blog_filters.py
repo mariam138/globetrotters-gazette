5ebf196842e3e25150d6c75f08c89d2f3ed9a4aa
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-25 19:32:38 +0000

    add custom filter to display region name

--- blog/templatetags/blog_filters.py
+++ blog/templatetags/blog_filters.py
@@ -1,7 +1,10 @@
-from ..models import Post
+from django import template
+from ..models import REGIONS
+
+register = template.Library()
 
 @register.filter
-def get_region_name(region_code):
+def get_region_name(region_code, is_safe=True):
     # Create a dictionary from the REGIONS choices list in models.py
     regions_dict = dict(REGIONS)
     # Will return the region name of the region_code that is specified
