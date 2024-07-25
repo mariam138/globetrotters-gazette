6b7bca8c653ad9fe1d215ef0fd89e8996050d307
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-25 13:10:38 +0000

    create post list class view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+# Allows use of built in generic views that Django supplies
+from django.views import generic
 from django.http import HttpResponse
 from django.contrib import messages
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -8,3 +11,9 @@ def index(request):
 
     # display success message when user has registered
     messages.add_message(request, messages.SUCCESS, 'Registration successful!')
+
+
+class PostsList(generic.ListView):
+    # Displays posts which have been categorised in the USA/ Canada region
+    queryset = Post.objects.filter(region='US')
+    template_name = "posts_list.html"
