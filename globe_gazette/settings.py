e5de1ebf8d996380810239f438bafd48ea4d8180
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-29 20:38:06 +0000

    maint: remove commented out db code

--- globe_gazette/settings.py
+++ globe_gazette/settings.py
@@ -116,13 +116,6 @@ WSGI_APPLICATION = 'globe_gazette.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 if os.path.isfile('env.py'):
     import env
 
