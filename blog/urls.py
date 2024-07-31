4c6492bcc326a18aaf0c13aaad84969c5d2f1f99
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-07-31 13:49:31 +0000

    create url for deleting posts

--- blog/urls.py
+++ blog/urls.py
@@ -16,5 +16,7 @@ urlpatterns = [
     path("create-post/cancel", views.cancel_create_post, name="cancel_post"),
     path("post/<slug:slug>/edit", views.edit_post, name="edit_post"),
     path("post/<slug:slug>/cancel-edit", views.cancel_edit_post, name="cancel_post_edit"),
+    path("post/<slug:slug>/delete", views.delete_post, name="delete_post"),
+
 
 ]
