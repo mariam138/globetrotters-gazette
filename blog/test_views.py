ec75239b25d483ab40a6aa7041314798933c6e54
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-01 14:39:21 +0000

    create class for testing index view

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestIndexView(TestCase):
+    def test_index_returns_200_code(self):
+
+        response = self.client.get('/')
+        assert response.status_code == 200
