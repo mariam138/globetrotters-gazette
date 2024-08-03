e2d746258c17b2f7a14c3a79d350d8ff7429f654
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-03 19:01:13 +0000

    override initialisation attrs for comment form

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+from crispy_forms.helper import FormHelper
 # Allows a form to be created from existing model in db
 from django.forms import ModelForm
 # Import Summernote widgets
@@ -28,3 +29,15 @@ class CommentForm(ModelForm):
         model = Comment
         # Only show the text field in the comment form
         fields = ["body",]
+
+    # Override form init method to hide form labels
+    # As only field from form that displays is the body
+    # Code adapted from:
+    # https://github.com/django-crispy-forms/django-crispy-forms/issues/248
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        # Sets id of the form
+        self.helper.form_id = 'commentForm'
+        # Hides form labels
+        self.helper.form_show_labels = False
