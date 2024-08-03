c473be9a22390a5464fec7688d5abe712e65ba88
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-03 11:38:59 +0000

    create url for delete account view

--- profile_page/urls.py
+++ profile_page/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("<str:username>/", views.profile_page, name="profile_page"),
     path("<str:username>/edit", views.edit_save_profile, name="edit_profile"),
     path("<str:username>/cancel", views.edit_cancel_profile, name="cancel_profile_changes"),
+    path("<str:username>/delete", views.delete_account, name="delete_account"),
+
 ]
