27938fb57e5e8c6c3e3abb35c3a3aabd51ad4464
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-04 15:52:55 +0000

    maint: add code comment for edit comment buttons

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,14 +1,3 @@
-/**
-* Initializes edit functionality for the provided edit buttons.
-* 
-* For each button in the `editButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Fetches the content of the corresponding comment.
-* - Populates the `commentText` input/textarea with the comment's content for editing.
-* - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
-*/
-
 // Gets all edit buttons on page by their class name
 let editButtons = document.getElementsByClassName('edit-btn');
 // Gets text of the comment where the id is the id of the form's text area
@@ -21,6 +10,17 @@ let submitButton = document.getElementById('submit-btn');
 let url = document.getElementById('url').getAttribute('data-url');
 let cancelCommentUrl = document.getElementById('cancel-comment-url').getAttribute('data-url');
 
+/**
+* Initializes edit functionality for the provided edit buttons.
+* 
+* For each button in the `editButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Fetches the content of the corresponding comment.
+* - Populates the `commentText` input/textarea with the comment's content for editing.
+* - Updates the submit button's text to "Update".
+* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+* Code adapted from https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+FSD101_WTS+4/courseware/713441aba05441dfb3a7cf04f3268b3f/21a16093c0084895a6073422447c3f7d/
+*/
 for (let button of editButtons){
     button.addEventListener('click', (e) => {
         let commentId = e.target.getAttribute('comment_id');
