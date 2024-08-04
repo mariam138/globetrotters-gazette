c9bf042c82a569b674135c39074ae5cb90018bec
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-04 19:38:13 +0000

    make custom approve posts action

--- blog/admin.py
+++ blog/admin.py
@@ -3,21 +3,38 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Comment
 
 
+
+#  Code to create own admin actions adopted from the Django docs
+# https://docs.djangoproject.com/en/4.2/ref/contrib/admin/actions/
+@admin.action(description="Approve selected posts")
+def approve_posts(modeladmin, request, queryset):
+    for post in queryset:
+        # Checks first if the post is published before approving it
+        if post.status == '1':
+            post.approved = True
+            # Saves object in database
+            post.save()
+
 class PostAdmin(SummernoteModelAdmin):
     # Prepopulates the slug field from the post's title
-    prepopulated_fields = {"slug": ("title",),}
+    prepopulated_fields = {"slug": ("title",), }
     # Allows searching by title and country
     search_fields = ['title', 'country']
     # Displays certain fields of model
-    list_display = ('title','country', 'user', 'created_on', 'status', 'approved',)
+    list_display = ('title', 'country', 'user',
+                    'created_on', 'status', 'approved',)
     # Chooses which fields to use as filtering
     list_filter = ('status', 'approved',)
     # Applies summernote editor to all Textfields
     summernote_fields = '__all__'
+    # Register custom actions defined above
+    actions = [approve_posts]
+
 
 @admin.action(description="Mark selected comments approved")
 def approve_comments(modeladmin, request, queryset):
     queryset.update(approved=True)
+
 
 class CommentAdmin(admin.ModelAdmin):
     # Displays fields in admin panel
@@ -26,6 +43,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ('approved',)
     # Registers custom actions defined above
     actions = [approve_comments]
+
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
