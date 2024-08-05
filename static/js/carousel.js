3cbed1718d47a0a24a77d38a15fa489c73d3b206
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-05 17:20:47 +0000

    initialise image carousel

---
+++ static/js/carousel.js
@@ -0,0 +1,7 @@
+// Initialise carousel
+// Code adapted from: https://getbootstrap.com/docs/5.3/components/carousel/#methods
+const indexCarousel = document.querySelector('#indexCarousel')
+
+const carousel = new bootstrap.Carousel(indexCarousel, {
+  ride: true
+})
