2fcdf6f09048d01d22293a3fea792cfb0b6edb89
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-05 20:26:29 +0000

    create About model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from profile_page.models import Profile
 
 # Create your models here.
+
+class About(models.Model):
+    """
+    Creates single instance of an About model in relation to
+    :model:`profile_page.Profile`
+    """
+    name = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    profile_pic = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return {self.name}
