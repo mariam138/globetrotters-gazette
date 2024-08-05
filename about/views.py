293146f7b5ec787289eda2e9b062d66300973c4b
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-05 21:02:31 +0000

    create about detail view

--- about/views.py
+++ about/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import About
 
 # Create your views here.
+class AboutDetailView(DetailView):
+    """
+    Uses Django's generic Detail view to display the About instance
+    """
+    model = About
+    template_name = "about/about.html"
+    context_object_name = "about"
