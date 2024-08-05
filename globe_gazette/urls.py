1644922fa287e4c9d69d1b0b77b4f74b769d2664
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-05 20:56:36 +0000

    include about app urls in project urls

--- globe_gazette/urls.py
+++ globe_gazette/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 # from blog import views
 urlpatterns = [
+    path('about/', include("about.urls"), name="about-urls"),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('profile/', include("profile_page.urls"), name="profile-urls"),
