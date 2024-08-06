4327967473e96101dc5de2817d668a217a30d324
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-06 11:06:40 +0000

    fix: change about view to function view

--- about/views.py
+++ about/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
+from django.shortcuts import render, get_object_or_404
+# from django.views.generic.detail import DetailView
 from .models import About
 
 # Create your views here.
@@ -11,3 +11,15 @@ from .models import About
 #     template_name = "about/about.html"
 #     context_object_name = "about"
 
+def about_detail(request):
+    """
+    Displays a single instance of :model:`about.About`
+    """
+    # Orders all about objects from most recently updated
+    # Then grabs the first instance only to be displayed
+    about = About.objects.all().order_by("-updated_on").first()
+    return render(
+        request,
+        "about/about.html",
+        {"about":about},
+    )
