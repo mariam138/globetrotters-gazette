e71859dc4ecd50b92d69fdedde35f46cfe6ec3e8
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-06 12:03:41 +0000

    maint: remove commented out code#

--- about/views.py
+++ about/views.py
@@ -2,15 +2,6 @@ from django.shortcuts import render, get_object_or_404
 # from django.views.generic.detail import DetailView
 from .models import About
 
-# Create your views here.
-# class AboutDetailView(DetailView):
-#     """
-#     Uses Django's generic Detail view to display the About instance
-#     """
-#     model = About
-#     template_name = "about/about.html"
-#     context_object_name = "about"
-
 def about_detail(request):
     """
     Displays a single instance of :model:`about.About`
