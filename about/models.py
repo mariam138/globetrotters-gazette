b167997cdfefc66f3aa1fbdefcdfc08b09494941
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-06 11:31:36 +0000

    update About model

--- about/models.py
+++ about/models.py
@@ -8,8 +8,9 @@ class About(models.Model):
     Creates single instance of an About model in relation to
     :model:`profile_page.Profile`
     """
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="admin_profile")
     title = models.CharField(max_length=250)
-    profile_pic = models.OneToOneField(Profile, on_delete=models.CASCADE, related_name="admin_pic")
+    profile_pic = models.URLField(blank=True)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
 
