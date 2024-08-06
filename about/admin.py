6c6a27036430d07b0f2374f06555778a77af9fce
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-06 12:16:22 +0000

    add summernote to admin for about model

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import About
 
+
+class AboutAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+
 # Register your models here.
-admin.site.register(About)
+admin.site.register(About, AboutAdmin)
