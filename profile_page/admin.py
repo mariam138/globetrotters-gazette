8ee1a0ce52fd5e43a03a34ab790d089b87d370db
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-06 19:59:34 +0000

    register quill in admin

--- profile_page/admin.py
+++ profile_page/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
 from .models import Profile
-
-# add summernote to Profile bio text field in admin panel
-class ProfileAdmin(SummernoteModelAdmin):
-    summernote_fields = '__all__'
 
 
 # Register your models here.
-admin.site.register(Profile, ProfileAdmin)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
