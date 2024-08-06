35b0c40e64c5e4aa3a3215efc776242fdc7bb7e6
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-06 16:08:25 +0000

    query admin's latest post

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 # from django.views.generic.detail import DetailView
 from .models import About
+from blog.models import Post
 
 def about_detail(request):
     """
@@ -9,8 +11,19 @@ def about_detail(request):
     # Orders all about objects from most recently updated
     # Then grabs the first instance only to be displayed
     about = About.objects.all().order_by("-updated_on").first()
+
+    # As admin was first user created, get admin by pk of 1
+    admin = User.objects.get(id=1)
+    # Create a queryset of all post objects that are published, approved
+    # and written by the admin
+    queryset = Post.objects.filter(status='1', approved=True, user=admin)
+    # Create instance of post which gets the latest post admin has published
+    post = queryset.order_by("-created_on").first()
+
+
     return render(
         request,
         "about/about.html",
-        {"about":about},
+        {"about":about,
+        "post": post},
     )
