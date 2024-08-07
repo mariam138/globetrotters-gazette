199d4ed384d35c22856abd3eab650605970c2e51
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-07 20:26:12 +0000

    maint: add tooltip code credit

--- static/js/tooltip.js
+++ static/js/tooltip.js
@@ -1,3 +1,5 @@
 // Get the button to generate the tooltip
+// Code adapted from Bootstrap documentation
+// https://getbootstrap.com/docs/5.3/components/tooltips/#usage
 let emailInfoButton = document.getElementById('email-tooltip');
 let emailTooltip = new bootstrap.Tooltip(emailInfoButton);
