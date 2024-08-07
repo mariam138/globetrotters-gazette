b3e389a2e603408bfd9be6cec71034f4c0e3a3d5
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-07 20:20:16 +0000

    maint: remove cloudinary script from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -10,48 +10,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     }
 
-    // // Code adapted from Cloudinary docs
-    // // https://cloudinary.com/documentation/upload_widget_reference#initialization_methods
-    // let cloudinaryWidget = cloudinary.createUploadWidget({
-    //         cloudName: "dy1xfelbe",
-    //         uploadPreset: "blog_image",
-    //         // Allow only one picture to be uploaded to be used as the hero image for the post
-    //         multiple: false,
-    //         // Allow the user to crop their pictures
-    //         cropping: true,
-    //         // Enforces a 16:9 cropping ratio so the image is suitable to be displayed as a hero image
-    //         croppingAspectRatio: 1.78,
-    //         // Only allows images to be uploaded
-    //         resourceType: "image",
-    //         // Specifies image formats allowed to client
-    //         clientAllowedFormats: ["jpg", "jpeg", "webp", "png",],
-    //         // Only allows images of 5MB to be uploaded, performs validation
-    //         maxImageFileSize: 5000000,
-    //         // Prevents widget from automatically closing after one file upload
-    //         singleUploadAutoClose: false,
-
-    //     },
-    //     function (error, result) {
-    //         // Getting the image url from the uploaded photo from the widget is adapted from:
-    //         // https://stackoverflow.com/questions/61153317/how-to-get-the-cloudinary-widget-image-info-on-upload
-    //         if (!error && result && result.event === "success") {
-    //             // Gets the hidden input field from the create post form
-    //             let imageUrl = document.getElementById('image_url');
-    //             // Sets the value of the hidden input form as the url of the picture uploaded
-    //             imageUrl.value = result.info.secure_url;
-
-    //             // Gets input field after upload button
-    //             let imageName = document.getElementById('image-name');
-    //             // Sets the placeholder text to the name of the image uploaded
-    //             imageName.placeholder = result.info.public_id;
-    //         }
-    //     });
-
-    // // Gets upload button from post creation page
-    // let uploadBlogImg = document.getElementById('upload-img');
-    // uploadBlogImg.addEventListener('click', () => {
-    //     cloudinaryWidget.open();
-    // }, false);
 });
 
 // Get the button to generate the tooltip
