3e9c2f5ba04013c94332758b0ed591c89f109568
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-07 20:32:33 +0000

    refactor: fix script for clicking on back buttons

---
+++ static/js/backButton.js
@@ -0,0 +1,7 @@
+// Get back button on post detail page
+let backButtons = document.getElementsByClassName('back-btn');
+
+// loop through back buttons as they are defined by class name
+for (button of backButtons) {
+    button.addEventListener('click', () => history.back());
+}
