7040f5923982051656cc545e97219e8891ae9355
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-09 13:25:26 +0000

    test post form validity

--- blog/tests/test_forms.py
+++ blog/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .forms import PostForm
+from blog.forms import PostForm
+import json
 # Create your tests here.
 
 class TestPostForm(TestCase):
@@ -7,11 +8,12 @@ class TestPostForm(TestCase):
     Includes tests on the :model:`blog.PostForm` model
     """
     def test_form_is_valid(self):
+        """ Tests form is valid """
         post_form = PostForm({
             'title': 'Test Title',
             'region': 'SA',
             'country': 'Colombia',
-            'body': 'This is a test form',
+            'body': json.dumps({"delta": {"ops": [{"insert": "This is a test form\n"}]}}),
             'status': 0,
         })
         self.assertTrue(post_form.is_valid())
