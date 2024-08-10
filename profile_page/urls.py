0e9b4261788ff24b9f74808e63c7a2182b881479
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-10 14:33:14 +0000

    add username to profile page url

--- profile_page/urls.py
+++ profile_page/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.profile_page, name="profile_page"),
+    path("<str:username>", views.profile_page, name="profile_page"),
     path("<str:username>/edit", views.edit_save_profile, name="edit_profile"),
     path("<str:username>/cancel", views.edit_cancel_profile, name="cancel_profile_changes"),
     path("<str:username>/delete", views.delete_account, name="delete_account"),
