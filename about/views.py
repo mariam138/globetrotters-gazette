b29eadbecc4201e709bad05f6aaa0fe01b612e23
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-08-10 13:37:56 +0000

    debug about instance

--- about/views.py
+++ about/views.py
@@ -3,17 +3,19 @@ from django.shortcuts import render, get_object_or_404
 # from django.views.generic.detail import DetailView
 from .models import About
 from blog.models import Post
+from profile_page.models import Profile
 
 def about_detail(request):
     """
     Displays a single instance of :model:`about.About`
     """
+
+    # As admin was first user created, get admin by pk of 1
+    admin = User.objects.get(id=1)
     # Orders all about objects from most recently updated
     # Then grabs the first instance only to be displayed
     about = About.objects.all().order_by("-updated_on").first()
 
-    # As admin was first user created, get admin by pk of 1
-    admin = User.objects.get(id=1)
     # Create a queryset of all post objects that are published, approved
     # and written by the admin
     queryset = Post.objects.filter(status='1', approved=True, user=admin)
